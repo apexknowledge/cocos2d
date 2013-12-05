7f4d4728aaa7c6ca2881272923b5ff7437983ec7
Howard Cantrell Jr <howardc@apexknowledge.com>
2013-12-05 15:52:21 -0600

    Updates to current SDK

--- cocos2d-android/src/org/cocos2d/nodes/CCAtlasNode.java
+++ cocos2d-android/src/org/cocos2d/nodes/CCAtlasNode.java
@@ -17,38 +17,35 @@ import javax.microedition.khronos.opengles.GL10;
  * <p/>
  * It knows how to render a TextureAtlas object.
  * <p/>
- * All features from CocosNode are valid, plus the following features:
- * - opacity
- * - color (setRGB:::)
+ * All features from CocosNode are valid, plus the following features: - opacity - color (setRGB:::)
  * - contentSize
  */
 
-/** CCAtlasNode is a subclass of CCNode that implements the CCRGBAProtocol and
- CCTextureProtocol protocol
- 
- It knows how to render a TextureAtlas object.
- If you are going to render a TextureAtlas consider subclassing CCAtlasNode (or a subclass of CCAtlasNode)
- 
- All features from CCNode are valid, plus the following features:
- - opacity and RGB colors
+/**
+ * CCAtlasNode is a subclass of CCNode that implements the CCRGBAProtocol and CCTextureProtocol
+ * protocol
+ * 
+ * It knows how to render a TextureAtlas object. If you are going to render a TextureAtlas consider
+ * subclassing CCAtlasNode (or a subclass of CCAtlasNode)
+ * 
+ * All features from CCNode are valid, plus the following features: - opacity and RGB colors
  */
-public abstract class CCAtlasNode extends CCNode
-        implements CCRGBAProtocol, CCTextureProtocol {
-    /// texture atlas
+public abstract class CCAtlasNode extends CCNode implements CCRGBAProtocol, CCTextureProtocol {
+    // / texture atlas
     protected CCTextureAtlas textureAtlas_;
-    /// chars per row
+    // / chars per row
     protected int itemsPerRow;
-    /// chars per column
+    // / chars per column
     protected int itemsPerColumn;
 
-    /// texture coordinate x increment
+    // / texture coordinate x increment
     protected float texStepX;
-    /// texture coordinate y increment
+    // / texture coordinate y increment
     protected float texStepY;
 
-    /// width of each char
+    // / width of each char
     protected int itemWidth;
-    /// height of each char
+    // / height of each char
     protected int itemHeight;
 
     // blend function
@@ -57,13 +54,16 @@ public abstract class CCAtlasNode extends CCNode
     // texture RGBA
     int opacity_;
     ccColor3B color_;
-	ccColor3B	colorUnmodified_;
+    ccColor3B colorUnmodified_;
     boolean opacityModifyRGB_;
 
-    /** initializes an CCAtlasNode  with an Atlas file the width and height of each item and the quantity of items to render*/
+    /**
+     * initializes an CCAtlasNode with an Atlas file the width and height of each item and the
+     * quantity of items to render
+     */
     protected CCAtlasNode(String tile, int w, int h, int c) {
-    	super();
-    	
+        super();
+
         itemWidth = w;
         itemHeight = h;
         opacity_ = 255;
@@ -76,36 +76,40 @@ public abstract class CCAtlasNode extends CCNode
 
         updateBlendFunc();
         updateOpacityModifyRGB();
-        
+
         calculateMaxItems();
         calculateTexCoordsSteps();
     }
 
     private void calculateMaxItems() {
         CGSize s = textureAtlas_.getTexture().getContentSize();
-        itemsPerColumn = (int) (s.height / itemHeight);
-        itemsPerRow = (int) (s.width / itemWidth);
+        if (s != null) {
+            itemsPerColumn = (int) (s.height / itemHeight);
+            itemsPerRow = (int) (s.width / itemWidth);
+        }
     }
 
     private void calculateTexCoordsSteps() {
         CCTexture2D tex = textureAtlas_.getTexture();
-        texStepX = itemWidth / (float) tex.pixelsWide();
-        texStepY = itemHeight / (float) tex.pixelsHigh();
+        if (tex != null) {
+            texStepX = itemWidth / (float) tex.pixelsWide();
+            texStepY = itemHeight / (float) tex.pixelsHigh();
+        }
     }
 
-    /** updates the Atlas (indexed vertex array).
-     * Shall be overriden in subclasses
+    /**
+     * updates the Atlas (indexed vertex array). Shall be overriden in subclasses
      */
     public abstract void updateAtlasValues();
 
     @Override
     public void draw(GL10 gl) {
 
-    	// Default GL states: GL_TEXTURE_2D, GL_VERTEX_ARRAY, GL_COLOR_ARRAY, GL_TEXTURE_COORD_ARRAY
-    	// Needed states: GL_TEXTURE_2D, GL_VERTEX_ARRAY, GL_TEXTURE_COORD_ARRAY
-    	// Unneeded states: GL_COLOR_ARRAY
-    	gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
-    	
+        // Default GL states: GL_TEXTURE_2D, GL_VERTEX_ARRAY, GL_COLOR_ARRAY, GL_TEXTURE_COORD_ARRAY
+        // Needed states: GL_TEXTURE_2D, GL_VERTEX_ARRAY, GL_TEXTURE_COORD_ARRAY
+        // Unneeded states: GL_COLOR_ARRAY
+        gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
+
         gl.glColor4f(color_.r / 255f, color_.g / 255f, color_.b / 255f, opacity_ / 255f);
 
         boolean newBlend = false;
@@ -119,23 +123,23 @@ public abstract class CCAtlasNode extends CCNode
         if (newBlend)
             gl.glBlendFunc(ccConfig.CC_BLEND_SRC, ccConfig.CC_BLEND_DST);
 
-       	
-    	// is this chepear than saving/restoring color state ?
-    	// XXX: There is no need to restore the color to (255,255,255,255). Objects should use the color
-    	// XXX: that they need
-        //    	glColor4ub( 255, 255, 255, 255);
+        // is this chepear than saving/restoring color state ?
+        // XXX: There is no need to restore the color to (255,255,255,255). Objects should use the
+        // color
+        // XXX: that they need
+        // glColor4ub( 255, 255, 255, 255);
 
-    	// restore default GL state
-    	gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+        // restore default GL state
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
     }
-    
+
     /** conforms to CCRGBAProtocol protocol */
     public void setOpacity(int opacity) {
         opacity_ = opacity;
 
-	    // special opacity for premultiplied textures
+        // special opacity for premultiplied textures
         if (opacityModifyRGB_) {
-            setColor(opacityModifyRGB_? colorUnmodified_ : color_);
+            setColor(opacityModifyRGB_ ? colorUnmodified_ : color_);
         }
     }
 
@@ -147,11 +151,11 @@ public abstract class CCAtlasNode extends CCNode
     public void setColor(ccColor3B color) {
         color_ = new ccColor3B(color);
         colorUnmodified_ = new ccColor3B(color);
-        if( opacityModifyRGB_ ){
-            color_.r = color.r * opacity_/255;
-            color_.g = color.g * opacity_/255;
-            color_.b = color.b * opacity_/255;
-        }	
+        if (opacityModifyRGB_) {
+            color_.r = color.r * opacity_ / 255;
+            color_.g = color.g * opacity_ / 255;
+            color_.b = color.b * opacity_ / 255;
+        }
     }
 
     public ccColor3B getColor() {
@@ -165,10 +169,10 @@ public abstract class CCAtlasNode extends CCNode
     // CocosNodeTexture protocol
     /** conforms to CCTextureProtocol protocol */
     public void updateBlendFunc() {
-    	if( ! (textureAtlas_.getTexture().hasPremultipliedAlpha() )) {
-    		blendFunc_.src = GL10.GL_SRC_ALPHA;
-    		blendFunc_.dst = GL10.GL_ONE_MINUS_SRC_ALPHA;
-    	}
+        if (!(textureAtlas_.getTexture().hasPremultipliedAlpha())) {
+            blendFunc_.src = GL10.GL_SRC_ALPHA;
+            blendFunc_.dst = GL10.GL_ONE_MINUS_SRC_ALPHA;
+        }
     }
 
     /** conforms to CCTextureProtocol protocol */
@@ -181,17 +185,16 @@ public abstract class CCAtlasNode extends CCNode
     public CCTexture2D getTexture() {
         return textureAtlas_.getTexture();
     }
-    
+
     public void setOpacityModifyRGB(boolean modify) {
-    	opacityModifyRGB_ = modify;
+        opacityModifyRGB_ = modify;
     }
 
     public boolean doesOpacityModifyRGB() {
-    	return opacityModifyRGB_;
+        return opacityModifyRGB_;
     }
 
     public void updateOpacityModifyRGB() {
-    	opacityModifyRGB_ = textureAtlas_.getTexture().hasPremultipliedAlpha();
+        opacityModifyRGB_ = textureAtlas_.getTexture().hasPremultipliedAlpha();
     }
 }
-
--- cocos2d-android/src/org/cocos2d/nodes/CCLabelAtlas.java
+++ cocos2d-android/src/org/cocos2d/nodes/CCLabelAtlas.java
@@ -10,33 +10,38 @@ import org.cocos2d.utils.javolution.TextBuilder;
 
 import javax.microedition.khronos.opengles.GL10;
 
-/** CCLabelAtlas is a subclass of CCAtlasNode.
- 
- It can be as a replacement of CCLabel since it is MUCH faster.
- 
- CCLabelAtlas versus CCLabel:
- - CCLabelAtlas is MUCH faster than CCLabel
- - CCLabelAtlas "characters" have a fixed height and width
- - CCLabelAtlas "characters" can be anything you want since they are taken from an image file
- 
- A more flexible class is CCBitmapFontAtlas. It supports variable width characters and it also has a nice editor.
+/**
+ * CCLabelAtlas is a subclass of CCAtlasNode.
+ * 
+ * It can be as a replacement of CCLabel since it is MUCH faster.
+ * 
+ * CCLabelAtlas versus CCLabel: - CCLabelAtlas is MUCH faster than CCLabel - CCLabelAtlas
+ * "characters" have a fixed height and width - CCLabelAtlas "characters" can be anything you want
+ * since they are taken from an image file
+ * 
+ * A more flexible class is CCBitmapFontAtlas. It supports variable width characters and it also has
+ * a nice editor.
  */
-public class CCLabelAtlas extends CCAtlasNode 
-	    implements CCLabelProtocol, CCNode.CocosNodeSize {
-    /// string to render
+public class CCLabelAtlas extends CCAtlasNode implements CCLabelProtocol, CCNode.CocosNodeSize {
+    // / string to render
     TextBuilder string_;
 
-    /// the first char in the charmap
+    // / the first char in the charmap
     char mapStartChar;
 
-    /** creates the CCLabelAtlas with a string,
-     * a char map file(the atlas), the width and height of each element
-     * and the starting char of the atlas */
-    public static CCLabelAtlas label(CharSequence theString, String charmapfile, int w, int h, char c) {
+    /**
+     * creates the CCLabelAtlas with a string, a char map file(the atlas), the width and height of
+     * each element and the starting char of the atlas
+     */
+    public static CCLabelAtlas label(CharSequence theString, String charmapfile, int w, int h,
+            char c) {
         return new CCLabelAtlas(theString, charmapfile, w, h, c);
     }
 
-    /** initializes the CCLabelAtlas with a string, a char map file(the atlas), the width and height of each element and the starting char of the atlas */
+    /**
+     * initializes the CCLabelAtlas with a string, a char map file(the atlas), the width and height
+     * of each element and the starting char of the atlas
+     */
     protected CCLabelAtlas(CharSequence theString, String charmapfile, int w, int h, char c) {
         super(charmapfile, w, h, theString.length());
 
@@ -52,40 +57,42 @@ public class CCLabelAtlas extends CCAtlasNode
         int n = string_.length();
 
         PoolHolder holder = PoolHolder.getInstance();
-        ccQuad2 texCoord = holder.getccQuad2Pool().get(); 
-        ccQuad3 vertex   = holder.getccQuad3Pool().get(); 
+        ccQuad2 texCoord = holder.getccQuad2Pool().get();
+        ccQuad3 vertex = holder.getccQuad3Pool().get();
 
-//        String s = string_;
-        for (int i = 0; i < n; i++) {
-            int a = string_.charAt(i) - mapStartChar;
-            float row = (a % itemsPerRow) * texStepX;
-            float col = (a / itemsPerRow) * texStepY;
+        // String s = string_;
+        if (itemsPerRow != 0) {
+            for (int i = 0; i < n; i++) {
+                int a = string_.charAt(i) - mapStartChar;
+                float row = (a % itemsPerRow) * texStepX;
+                float col = (a / itemsPerRow) * texStepY;
 
-            texCoord.bl_x = row;                        // A - x
-            texCoord.bl_y = col;                        // A - y
-            texCoord.br_x = row + texStepX;                // B - x
-            texCoord.br_y = col;                        // B - y
-            texCoord.tl_x = row;                        // C - x
-            texCoord.tl_y = col + texStepY;                // C - y
-            texCoord.tr_x = row + texStepX;                // D - x
-            texCoord.tr_y = col + texStepY;                // D - y
+                texCoord.bl_x = row; // A - x
+                texCoord.bl_y = col; // A - y
+                texCoord.br_x = row + texStepX; // B - x
+                texCoord.br_y = col; // B - y
+                texCoord.tl_x = row; // C - x
+                texCoord.tl_y = col + texStepY; // C - y
+                texCoord.tr_x = row + texStepX; // D - x
+                texCoord.tr_y = col + texStepY; // D - y
 
-            vertex.bl_x = i * itemWidth;                // A - x
-            vertex.bl_y = 0;                            // A - y
-            vertex.bl_z = 0;                            // A - z
-            vertex.br_x = i * itemWidth + itemWidth;    // B - x
-            vertex.br_y = 0;                            // B - y
-            vertex.br_z = 0;                            // B - z
-            vertex.tl_x = i * itemWidth;                // C - x
-            vertex.tl_y = itemHeight;                    // C - y
-            vertex.tl_z = 0;                            // C - z
-            vertex.tr_x = i * itemWidth + itemWidth;    // D - x
-            vertex.tr_y = itemHeight;                    // D - y
-            vertex.tr_z = 0;                            // D - z
+                vertex.bl_x = i * itemWidth; // A - x
+                vertex.bl_y = 0; // A - y
+                vertex.bl_z = 0; // A - z
+                vertex.br_x = i * itemWidth + itemWidth; // B - x
+                vertex.br_y = 0; // B - y
+                vertex.br_z = 0; // B - z
+                vertex.tl_x = i * itemWidth; // C - x
+                vertex.tl_y = itemHeight; // C - y
+                vertex.tl_z = 0; // C - z
+                vertex.tr_x = i * itemWidth + itemWidth; // D - x
+                vertex.tr_y = itemHeight; // D - y
+                vertex.tr_z = 0; // D - z
 
-            textureAtlas_.updateQuad(texCoord, vertex, i);
+                textureAtlas_.updateQuad(texCoord, vertex, i);
+            }
         }
-        
+
         holder.getccQuad2Pool().free(texCoord);
         holder.getccQuad3Pool().free(vertex);
     }
@@ -107,32 +114,27 @@ public class CCLabelAtlas extends CCAtlasNode
         // Needed states: GL_TEXTURE_2D, GL_VERTEX_ARRAY, GL_TEXTURE_COORD_ARRAY
         // Unneeded states: GL_COLOR_ARRAY
         gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
-	    gl.glColor4f(color_.r/255.f, color_.g/255.f, color_.b/255.f, opacity_/255.f);
+        gl.glColor4f(color_.r / 255.f, color_.g / 255.f, color_.b / 255.f, opacity_ / 255.f);
 
         boolean newBlend = false;
-        if( blendFunc_.src != ccConfig.CC_BLEND_SRC || blendFunc_.dst != ccConfig.CC_BLEND_DST ) {
+        if (blendFunc_.src != ccConfig.CC_BLEND_SRC || blendFunc_.dst != ccConfig.CC_BLEND_DST) {
             newBlend = true;
-            gl.glBlendFunc( blendFunc_.src, blendFunc_.dst );
+            gl.glBlendFunc(blendFunc_.src, blendFunc_.dst);
         }
 
         textureAtlas_.draw(gl, string_.length());
-	
-	    if( newBlend )
-		    gl.glBlendFunc(ccConfig.CC_BLEND_SRC, ccConfig.CC_BLEND_DST);
-	
-	    // Restore Default GL state. Enable GL_COLOR_ARRAY
-	    gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+
+        if (newBlend)
+            gl.glBlendFunc(ccConfig.CC_BLEND_SRC, ccConfig.CC_BLEND_DST);
+
+        // Restore Default GL state. Enable GL_COLOR_ARRAY
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
 
         /*
-        if (ccConfig.CC_LABELATLAS_DEBUG_DRAW) {
-            CGSize s = [self contentSize];
-            CGPoint vertices[4]={
-                ccp(0,0),ccp(s.width,0),
-                ccp(s.width,s.height),ccp(0,s.height),
-            };
-            ccDrawPoly(vertices, 4, YES);
-        } // CC_LABELATLAS_DEBUG_DRAW
-        */
+         * if (ccConfig.CC_LABELATLAS_DEBUG_DRAW) { CGSize s = [self contentSize]; CGPoint
+         * vertices[4]={ ccp(0,0),ccp(s.width,0), ccp(s.width,s.height),ccp(0,s.height), };
+         * ccDrawPoly(vertices, 4, YES); } // CC_LABELATLAS_DEBUG_DRAW
+         */
     }
 
     @Override
@@ -145,16 +147,15 @@ public class CCLabelAtlas extends CCAtlasNode
         return itemHeight;
     }
 
-	@Override
-	public ccBlendFunc getBlendFunc() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public ccBlendFunc getBlendFunc() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void setBlendFunc(ccBlendFunc blendFunc) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void setBlendFunc(ccBlendFunc blendFunc) {
+        // TODO Auto-generated method stub
+
+    }
 }
-
